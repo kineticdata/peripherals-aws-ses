c9dbcbabb66d2f4b8abdb98514c205f946750287
McClain Looney <m@loonsoft.com>
2023-05-31 11:37:17 -0500

    gradle conversion & snyk updates

---
+++ bridge-adapters/kinetic-bridgehub-adapter-amazonses/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+repositories {
+  mavenLocal()
+  maven {
+    url = uri("https://s3.amazonaws.com/maven-repo-public-kineticdata.com/releases")
+  }
+
+  maven {
+    url = uri("s3://maven-repo-private-kineticdata.com/releases")
+    authentication {
+      create<AwsImAuthentication>("awsIm")
+    }
+
+  }
+
+  maven {
+    url = uri("https://s3.amazonaws.com/maven-repo-public-kineticdata.com/snapshots")
+  }
+
+  maven {
+    url = uri("s3://maven-repo-private-kineticdata.com/snapshots")
+    authentication {
+      create<AwsImAuthentication>("awsIm")
+    }
+  }
+
+  maven {
+    url = uri("https://repo.maven.apache.org/maven2/")
+  }
+
+  maven {
+    url = uri("https://repo.springsource.org/release/")
+  }
+
+}
+
+dependencies {
+  implementation("org.apache.httpcomponents:httpclient:4.5.1")
+  implementation("com.kineticdata.bridges.adapter:kinetic-bridgehub-adapter:1.2.1-SNAPSHOT")
+  implementation("org.json:json:20230227")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.12.6.1")
+}
+
+group = "com.kineticdata.bridges.adapter"
+version = "1.0.1-SNAPSHOT"
+description = "kinetic-bridgehub-adapter-amazonses"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+publishing {
+  publications.create<MavenPublication>("maven") {
+    from(components["java"])
+  }
+  repositories {
+    maven {
+      val releasesUrl = uri("s3://maven-repo-private-kineticdata.com/releases")
+      val snapshotsUrl = uri("s3://maven-repo-private-kineticdata.com/snapshots")
+      url = if (version.toString().endsWith("SNAPSHOT")) snapshotsUrl else releasesUrl
+      authentication {
+        create<AwsImAuthentication>("awsIm")
+      }
+    }
+  }
+}
+
+tasks.withType<JavaCompile>() {
+  options.encoding = "UTF-8"
+}
